68b607bee2acd7f3045ff9b989824a72894e6b6d
Savar Bhasin <51899822+savarbhasin@users.noreply.github.com>
2024-08-16 00:38:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if radio == "GPT-4o":
     model = ChatOpenAI(model="gpt-4o", api_key=openapi_key)
 
 
-if st.button("Generate Chapters (currently works only with transcribed videos)"):
+if st.button("Generate Chapters"):
     if radio == "GPT-4o" and not openapi_key:
         st.error("Please enter your OpenAI API key.")
         st.stop()
